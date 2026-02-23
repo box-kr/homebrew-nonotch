daa5712311e1e309981907c2a5c961ffd291a25f
Andrew jeon <andrew.jeon@box.co.kr>
2026-02-23 11:07:12 +0900

    Update NoNotch to v1.0.17

--- Casks/nonotch.rb
+++ Casks/nonotch.rb
@@ -1,6 +1,6 @@
 cask "nonotch" do
-  version "1.0.16"
-  sha256 "981b249aca694d8e5ecc1e86386508021949af596ae5c71eee6faa77c941341b"
+  version "1.0.17"
+  sha256 "02b8ffd3487960338d255f16afb427a2a6e36514a05147fa0d3028383a970363"
 
   url "https://github.com/box-kr/no_notch/releases/download/v#{version}/NoNotch-#{version}.zip"
   name "NoNotch"
@@ -8,6 +8,14 @@ cask "nonotch" do
   homepage "https://github.com/box-kr/no_notch"
 
   app "NoNotch.app"
+
+  # 다운로드 후 Gatekeeper에서 '손상된 앱'으로 인식되는 것을 방지합니다.
+  postflight do
+    system_command "xattr",
+                   args: ["-cr", "#{appdir}/NoNotch.app"],
+                   sudo: false,
+                   must_succeed: false
+  end
 
   zap trash: [
     "~/Library/Preferences/com.box-kr.NoNotch.plist",
