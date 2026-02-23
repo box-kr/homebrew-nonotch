58414e1b3fe48129f33477fc206707b03b157ecb
Andrew jeon <andrew.jeon@box.co.kr>
2026-02-23 10:50:35 +0900

    Update NoNotch to v1.0.20

--- Casks/nonotch.rb
+++ Casks/nonotch.rb
@@ -1,6 +1,6 @@
 cask "nonotch" do
-  version "1.0.9"
-  sha256 "5028cf6d2b11fc04970a901d7a9d2b16b9c93d37c9129eef2805bd1dabfa48ef"
+  version "1.0.20"
+  sha256 "8117f907e5fbc3b2e64aee03351b11c257645aafd152504290d99303fd370685"
 
   url "https://github.com/box-kr/no_notch/releases/download/v#{version}/NoNotch-#{version}.zip"
   name "NoNotch"
